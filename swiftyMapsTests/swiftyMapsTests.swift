f736decea1e5ce4a8cf30228b5fc2f991c01b010
Prateek <prateek@incube8.sg>
2018-04-28 13:17:33 +0800

    Initial Commit

---
+++ swiftyMapsTests/swiftyMapsTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftyMapsTests.swift
+//  swiftyMapsTests
+//
+//  Created by Prateek on 28/4/18.
+//  Copyright © 2018 Prateek. All rights reserved.
+//
+
+import XCTest
+@testable import swiftyMaps
+
+class swiftyMapsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
